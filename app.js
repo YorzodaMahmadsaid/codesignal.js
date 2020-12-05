ae4650ed1146116726d30f8eeda434155019476e
Yorzoda Mahmadsaid <48682215+YorzodaMahmadsaid@users.noreply.github.com>
2020-12-05 10:45:12 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-Write a function that returns the sum of two numbers.
+/* Write a function that returns the sum of two numbers.
 
 Example
 
 For param1 = 1 and param2 = 2, the output should be
-add(param1, param2) = 3.
+add(param1, param2) = 3. */
 
 function add(param1, param2) {
    return param1 + param2;
@@ -13,7 +13,7 @@ add(1, 2);
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-Given a year, return the century it is in. 
+/* Given a year, return the century it is in. 
 The first century spans from the year 1 up to and including the year 100, 
 the second - from the year 101 up to and including the year 200, etc.
 
@@ -22,7 +22,7 @@ Example
 For year = 1905, the output should be
 centuryFromYear(year) = 20;
 For year = 1700, the output should be
-centuryFromYear(year) = 17.
+centuryFromYear(year) = 17.*/
 
 function centuryFromYear(year) {
     let inputYear = Math.floor(year/100);
