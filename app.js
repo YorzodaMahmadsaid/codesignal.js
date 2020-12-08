71e039a3b7e0dfa772b3d45afdd946263916bc2a
Yorzoda Mahmadsaid <48682215+YorzodaMahmadsaid@users.noreply.github.com>
2020-12-08 17:59:21 +0500

    Update app.js

--- app.js
+++ app.js
@@ -36,3 +36,21 @@ function centuryFromYear(year) {
 centuryFromYear(1905);
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+/* Given an array of integers, find the pair of adjacent elements that has the largest product and return that product.
+Example
+For inputArray = [3, 6, -2, -5, 7, 3], the output should be
+adjacentElementsProduct(inputArray) = 21.
+7 and 3 produce the largest product */
+
+function adjacentElementsProduct(inputArray) {
+    let maxAdjElement = 0;
+    for(let i = 0; i < inputArray.length-1; i++) {
+        if(maxAdjElement < inputArray[i]*inputArray[i+1]) {
+            maxAdjElement = inputArray[i]*inputArray[i+1];
+        }
+    }
+    return maxAdjElement;
+}
+
+console.log(adjacentElementsProduct([2,5,1,6,9,2]));
